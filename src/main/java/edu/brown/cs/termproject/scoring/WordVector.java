6d2be559cc437ba0532bf072236d1f01016774e7
asekula <alex@Alexs-MacBook-Air-2.local>
2017-05-02 23:20:10 -0400

    removed nearest code

--- src/main/java/edu/brown/cs/termproject/scoring/WordVector.java
+++ src/main/java/edu/brown/cs/termproject/scoring/WordVector.java
@@ -16,7 +16,7 @@ import java.util.Set;
  * safe. This is because the vector is stored as an immutable list and the
  * reference itself never changes, so the whole class is immutable.
  */
-class WordVector implements Cartesian<Double> {
+class WordVector {
 
   // All fields are immutable.
   private Optional<ImmutableList<Double>> vector;
@@ -150,43 +150,5 @@ class WordVector implements Cartesian<Double> {
   @Override
   public int hashCode() {
     return word.hashCode();
-  }
-
-  // Code for Cartesian<Double> starts here.
-  @Override
-  public Double get(int dimension) {
-    if (dimension < 0 || dimension >= vector.get().size()) {
-      return 0.0;
-    }
-
-    return vector.get().get(dimension);
-  }
-
-  @Override
-  public Double squaredDistanceTo(Cartesian<Double> point) {
-    double total = 0;
-    int maxDimension = Math.max(point.getDimension(), getDimension());
-
-    for (int i = 0; i < maxDimension; i++) {
-      total += Math.pow(point.get(i) - get(i), 2);
-    }
-    return total;
-  }
-
-  @Override
-  public int compareAtCoordinate(Cartesian<Double> other, int coordinate) {
-    return Double.compare(get(coordinate), other.get(coordinate));
-  }
-
-  @Override
-  public Double squaredDifferenceAtCoordinate(Cartesian<Double> other,
-      int coordinate) {
-    double ex = get(coordinate) - other.get(coordinate);
-    return ex * ex;
-  }
-
-  @Override
-  public int getDimension() {
-    return vector.get().size();
   }
 }

a7de6f880f4cca004252926a34522d05d062a975
asekula <alex@Alexs-MacBook-Air-2.local>
2017-05-04 00:05:55 -0400

    scoring documentation

--- src/main/java/edu/brown/cs/termproject/scoring/SuggestionThresholdFinder.java
+++ src/main/java/edu/brown/cs/termproject/scoring/SuggestionThresholdFinder.java
@@ -128,12 +128,12 @@ final class SuggestionThresholdFinder {
         for (int j = i + 1; j < words.size(); j++) {
           if (words.get(i).similarity(words.get(j)) >= threshold) {
             correct += priority;
-            if (Math.abs(threshold - 1.43) < 0.01) {
+            if (Math.abs(threshold - 1.4) < 0.01) {
               System.out
                   .println("Clustered: " + words.get(i) + ":" + words.get(j));
             }
           } else {
-            if (Math.abs(threshold - 1.43) < 0.01) {
+            if (Math.abs(threshold - 1.4) < 0.01) {
               System.out.println(
                   "Didn't cluster: " + words.get(i) + ":" + words.get(j));
             }
